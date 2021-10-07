41c16772c9cccbbaf86fd1fe78dcce416d5ceca3
Pedro Lisboa <phlisboa2000@gmail.com>
2021-10-07 17:47:16 -0300

    Atualização do curso.

---
+++ 14 - Jupyter/main.py
@@ -0,0 +1,9 @@
+"""
+Jupyter notebook é uma aplicação web que permite a execução de código em tempo real,
+visualização de dados e texto explicativos.
+Cientistas de dados podem criar, executar o código e explicar o processo enquanto executam o código e mostram os
+dados.
+
+pip install jupyter
+pipenv install jupyter
+"""
